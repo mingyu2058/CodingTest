ee06b6a9cf601238f750d2440699c69608247db2
조민규 <inch1209@naver.com>
2022-01-12 21:14:44 +0900

    programmers-CaesarCipher
    https://programmers.co.kr/learn/courses/30/lessons/12926

---
+++ programmers-codingtest/CaesarCipher.java
@@ -0,0 +1,40 @@
+/**
+ * 문제: 시저 암호
+ * 링크 : https://programmers.co.kr/learn/courses/30/lessons/12926
+ */
+
+public class CaesarCipher {
+    public static void main(String args[]){
+        //solution("AB",1);
+        solution("a B z",4);
+
+    }
+    static String solution(String s, int n){
+        char[] result;
+        result = s.toCharArray();
+
+        for(int i=0; i<result.length; i++) {
+            int tmp = n % 26;
+            if(result[i]>=65&&result[i]<=90) {
+                if (result[i] + tmp >= 91)
+                    result[i] = (char) (result[i] + tmp - 90 + 64);
+                else
+                    result[i] = (char) (result[i] + tmp);
+
+            }
+
+            if(result[i]>=97&&result[i]<=122){
+                if(result[i]+n%26>=123) {
+                    result[i] = (char) (result[i] + tmp-122+96);
+                }
+                else
+                    result[i] = (char) (result[i]+tmp);
+            }
+
+        }
+        String x = String.valueOf(result);
+        System.out.print(x);
+
+        return ("x");
+    }
+}
