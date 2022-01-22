e9ffe570e092095ab815b31da9042709fb1209af
조민규 <inch1209@naver.com>
2022-01-22 23:51:36 +0900

    문자열 내림차순으로 배치하기
    inch1@LAPTOP-LGFO4646 MINGW64 ~/codingtest-workspace (master)
    $ git add .
    
    inch1@LAPTOP-LGFO4646 MINGW64 ~/codingtest-workspace (master)
    $ git log
    commit 6322f030a107f6f7b3b887a4dd460583680cc64c (HEAD -> master, origin/master)
    Author: 조민규 <inch1209@naver.com>
    Date:   Fri Jan 21 23:52:08 2022 +0900
    
        programmers-DescendingOrderString
    
        https://programmers.co.kr/learn/courses/30/lessons/12917
    
    commit f75cd2be7b9f02923b06e1f4a8b45ea78bcf32d2
    Author: 조민규 <inch1209@naver.com>
    Date:   Thu Jan 20 23:13:18 2022 +0900
    
        programmers-HandleString
    
        https://programmers.co.kr/learn/courses/30/lessons/12918
    
    commit 862d924712fcb418410a6ece150d294952ee5707
    Author: 조민규 <inch1209@naver.com>
    Date:   Thu Jan 20 22:50:33 2022 +0900
    
        programmers-FindKim
    
        https://programmers.co.kr/learn/courses/30/lessons/12919
    
    inch1@LAPTOP-LGFO4646 MINGW64 ~/codingtest-workspace (master)
    $ git commit -m programmers-BetweenNumber -m https://programmers.co.kr/learn/courses/30/lessons/12912
    [master ad24a1c] programmers-BetweenNumber
     6 files changed, 157 insertions(+), 1 deletion(-)
     create mode 100644 programmers-codingtest/.idea/uiDesigner.xml
     create mode 100644 programmers-codingtest/BetweenNumber.java
     create mode 100644 programmers-codingtest/out/production/programmers-codingtest/BetweenNumber.class
     create mode 100644 programmers-codingtest/out/production/programmers-codingtest/DescendingOrderString.class
     delete mode 100644 programmers-codingtest/out/production/programmers-codingtest/InverseString.class
    
    inch1@LAPTOP-LGFO4646 MINGW64 ~/codingtest-workspace (master)
    $ git push orign master
    fatal: 'orign' does not appear to be a git repository
    fatal: Could not read from remote repository.
    
    Please make sure you have the correct access rights
    and the repository exists.
    
    inch1@LAPTOP-LGFO4646 MINGW64 ~/codingtest-workspace (master)
    $ git origin master
    git: 'origin' is not a git command. See 'git --help'.
    
    inch1@LAPTOP-LGFO4646 MINGW64 ~/codingtest-workspace (master)
    $ git psu horigin master
    git: 'psu' is not a git command. See 'git --help'.
    
    The most similar command is
            push
    
    inch1@LAPTOP-LGFO4646 MINGW64 ~/codingtest-workspace (master)
    $ git push horigin master
    fatal: 'horigin' does not appear to be a git repository
    fatal: Could not read from remote repository.
    
    Please make sure you have the correct access rights
    and the repository exists.
    
    inch1@LAPTOP-LGFO4646 MINGW64 ~/codingtest-workspace (master)
    $ git push origin master
    Enumerating objects: 18, done.
    Counting objects: 100% (18/18), done.
    Delta compression using up to 8 threads
    Compressing objects: 100% (10/10), done.
    Writing objects: 100% (12/12), 3.89 KiB | 1.94 MiB/s, done.
    Total 12 (delta 4), reused 0 (delta 0), pack-reused 0
    remote: Resolving deltas: 100% (4/4), completed with 4 local objects.
    To https://github.com/mingyu2058/CodingTest.git
       6322f03..ad24a1c  master -> master
    
    inch1@LAPTOP-LGFO4646 MINGW64 ~/codingtest-workspace (master)
    $ git add .
    
    inch1@LAPTOP-LGFO4646 MINGW64 ~/codingtest-workspace (master)
    $ git commit -m programmers-DescendingOrderString -m https://programmers.co.kr/learn/courses/30/lessons/12917 -m 설명 수정

--- programmers-codingtest/DescendingOrderString.java
+++ programmers-codingtest/DescendingOrderString.java
@@ -1,3 +1,9 @@
+/**
+ * 문제 : 문자열 내림차순으로 배치하기
+ * 링크 : https://programmers.co.kr/learn/courses/30/lessons/12917
+ */
+
+
 public class DescendingOrderString {
     public static void main(String args[]){
         String s;
