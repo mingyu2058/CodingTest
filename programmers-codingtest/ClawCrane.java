7d4379977801d31471ade9a7403838bfb96d9433
조민규 <inch1209@naver.com>
2022-01-30 14:11:02 +0900

    programmers-ClawCrane
    https://programmers.co.kr/learn/courses/30/lessons/64061
    
    문제해결

--- programmers-codingtest/ClawCrane.java
+++ programmers-codingtest/ClawCrane.java
@@ -11,48 +11,51 @@ public class ClawCrane {
         int board[][] = {{0, 0, 0, 0, 0}, {0, 0, 1, 0, 3}, {0, 2, 5, 0, 1}, {4, 2, 4, 4, 2}, {3, 5, 1, 3, 1}};
         int moves[] = {1, 5, 3, 5, 1, 2, 1, 4};
 
-
         int result = solution(board, moves);
         System.out.println("result = " + result);
     }
 
     public static int solution(int[][] board, int[] moves){
         int answer = 0, tmp;
-        int number;
+        int number = 0;
         int[] count = new int[board.length];
         Stack<Integer> stack = new Stack<Integer>();
 
-        for(int cnt :count) {
-            cnt = 0;
+        for(int i = 0; i< board.length; i++){
+            for(int j=0; j<board.length; j++){
+                if(board[j][i]!=0) {
+                    count[i] = j;
+                    break;
+                }
+            }
         }
 
-
         for(int move : moves){
-            move -=1;
-            number = board[count[move]][move];
-            count[move]++;
 
+            if(count[move-1]==board.length)
+                continue;;
+            number = board[count[move-1]][move-1];
 
-            if(stack.empty())
+            if(number==0)
+                continue;
+
+            count[move-1]++;
+
+            if(stack.empty()) {
                 stack.push(number);
+            }
             else {
                 tmp = stack.pop();
                 if (tmp != number) {
                     stack.push(tmp);
                     stack.push(number);
-
-                    System.out.println("stack = " + stack);
                 }
                 else{
                     answer+=2;
                 }
             }
 
-
-
         }
-
-
 
         return answer;
     }
