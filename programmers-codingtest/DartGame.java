e15ae9c6c08ad9d26cc33d6af4df950cf5ed2e19
조민규 <inch1209@naver.com>
2022-02-02 01:48:25 +0900

    programmers-DartGame
    https://programmers.co.kr/learn/courses/30/lessons/17682
    
    문제해결

--- programmers-codingtest/DartGame.java
+++ programmers-codingtest/DartGame.java
@@ -6,52 +6,91 @@
 public class DartGame {
     public static void main(String args[]){
         String dartResult;
-        dartResult = "1S2D*3T";  // 결과 37
-        //dartResult = "1D2S#10S"; // 결과 9
+        //dartResult = "1S2D*3T";  // 결과 37
+        dartResult = "1D2S#10S"; // 결과 9
         //dartResult = "1D2S0T"; //결과 3
+        //dartResult = "1S*2T*3S"; // 결과 23
+        //dartResult = "1T2D3D#"; // 결과 -4
+        //dartResult = "1D2S3T*"; // 결과 59
+        //dartResult = "10S10S10S";
+        System.out.println(solution(dartResult));
 
-        //System.out.println(solution(dartResult));
-
-        System.out.println('0'+1);
-        // 0~10 : 48~57
-        // S : 83,  D : 68,  T : 84
     }
     public static int solution(String dartResult){
-        int answer = 0, scoreTmp = 0, bonusTmp = 0, optionTmp = 0;
+        int answer = 0, scoreTmp = 0, bonusTmp = 0, optionTmp = 0, tmp=0;
         int score[] = new int[3];
         char bonus[] = new char[3];
-        char option[] = new char[3];
+        char option[] = {' ',' ',' '};
 
         char[] charDart = dartResult.toCharArray();
+        tmp = 0;
 
-        for(char ch : charDart){
-            if(ch>=65&&ch<=91){
-                score[scoreTmp] = (int) ch;
+
+        for(int i=0; i<9; i++){
+            if(i%3==0 && (charDart[tmp]>='0'&&charDart[tmp]<='9')) {
+                score[scoreTmp] = charDart[tmp++]- 48;
+
+                if(charDart[tmp]>='0'&&charDart[tmp]<='9') {
+                    System.out.println("tmp2 = " + tmp);
+                    score[scoreTmp] = 10;
+                    tmp++;
+                }
                 scoreTmp++;
-            } else if (ch=='S'||ch=='D'||ch=='T') {
-                bonus[bonusTmp] = ch;
+
+            }
+            else if(i%3==1 && (charDart[tmp]=='S'||charDart[tmp]=='D'||charDart[tmp]=='T')){
+                bonus[bonusTmp] = charDart[tmp++];
                 bonusTmp++;
             }
             else{
-                option[optionTmp] = ch;
-                optionTmp++;
+                if(i%3==2 && (charDart[tmp]=='*' || charDart[tmp]=='#')) {
+                    option[optionTmp] = charDart[tmp++];
+                    optionTmp++;
+                    continue;
+                }
+                else if(i%3==2){
+                    option[optionTmp] = charDart[tmp];
+                    optionTmp++;
+                }
             }
+
+            if(tmp==dartResult.length())
+                break;
         }
+
 
         for(int i=0; i<3; i++){
             switch (bonus[i]){
                 case 'S':
                     break;
                 case 'D':
+                    score[i]*=score[i];
                     break;
                 case 'T':
+                    score[i] = score[i]*score[i]*score[i];
                     break;
-
+            }
+            switch (option[i]){
+                case '*':
+                    if(i==0)
+                        score[i]*=2;
+                    else {
+                        score[i] *= 2;
+                        score[i - 1] *= 2;
+                    }
+                    break;
+                case '#':
+                    score[i]*=-1;
+                    break;
+                case ' ':
+                    break;
             }
 
 
         }
-
+        for(int num : score) {
+            answer += num;
+        }
 
         return answer;
 
