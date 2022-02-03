fa61ea3b7b0903fd7a71da154c811d230fb7894e
조민규 <inch1209@naver.com>
2022-02-03 23:57:33 +0900

    programmers-SumNoneNumber
    https://programmers.co.kr/learn/courses/30/lessons/86051

---
+++ programmers-codingtest/SumNoneNumber.java
@@ -0,0 +1,34 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+/**
+ * 문제: 없는 숫자 더하기
+ * 링크 : https://programmers.co.kr/learn/courses/30/lessons/86051
+ */
+
+public class SumNoneNumber {
+    public static void main(String args[]){
+        int[] numbers={1,2,3,4,6,7,8,0};
+        //int[] numbers = {5, 8, 4, 0, 6, 7, 9};
+
+        System.out.println(solution(numbers));
+
+    }
+    public static int solution(int []numbers){
+        int answer = 0;
+
+        ArrayList<Integer> numberList = new ArrayList<>();
+
+        for(int number : numbers)
+            numberList.add(number);
+
+        for(int i=0; i<10; i++){
+            if(!numberList.contains(i))
+                answer+=i;
+        }
+
+        return answer;
+    }
+
+
+}
