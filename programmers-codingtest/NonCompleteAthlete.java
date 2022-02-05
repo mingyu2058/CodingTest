ec65b85ba5e1dca4ae300c35b843b676436543c9
조민규 <inch1209@naver.com>
2022-02-06 01:02:11 +0900

    programmers-NonCompleteAthlete
     술려선  추가

--- programmers-codingtest/NonCompleteAthlete.java
+++ programmers-codingtest/NonCompleteAthlete.java
@@ -17,9 +17,30 @@ public class NonCompleteAthlete {
         String[] completion = {"stanko", "ana", "mislav"};
 
         System.out.println(solution(participant,completion));
+        System.out.println(solution2(participant,completion));
     }
 
+    // 효율성 테스트 : 42.37ms/68.61ms/70.25ms/92.24ms/79.80ms
     public static String solution(String[] participant, String[] completion){
+        String answer = "";
+        int tmp;
+        Map<String, Integer> answerMap = new HashMap<>();
+
+        for(String str : participant) answerMap.put(str,answerMap.getOrDefault(str,0)+1);
+        for(String str : completion) answerMap.put(str,answerMap.get(str)-1);
+
+        for(String key : answerMap.keySet()){
+            if(answerMap.get(key)!=0) {
+                answer = key;
+                break;
+            }
+        }
+        return answer;
+
+    }
+
+    // 효율성 테스트 : 32.74ms/62.02ms/44.34ms/52.30ms/89.86ms
+    public static String solution2(String[] participant, String[] completion){
         String answer = "";
         int tmp;
         Map<String, Integer> answerMap = new HashMap<>();
@@ -33,20 +54,17 @@ public class NonCompleteAthlete {
                 answerMap.put(str, 1);
             }
         }
-
         for(String str : participant) {
             if(!answerMap.containsKey(str)|| answerMap.get(str)==0){
-                answer =str;
+                answer = str;
                 break;
             }
             else{
                 tmp = answerMap.get(str) -1;
                 answerMap.put(str,tmp);
             }
-
         }
 
         return answer;
-        
     }
 }
