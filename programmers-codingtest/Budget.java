3837747eb51a47a157ad8c4ffae0f9c0320d839c
조민규 <inch1209@naver.com>
2022-02-17 00:14:36 +0900

    programmers-Budget
    https://programmers.co.kr/learn/courses/30/lessons/12982
    
    Arrays.sort(d)

--- programmers-codingtest/Budget.java
+++ programmers-codingtest/Budget.java
@@ -1,11 +1,45 @@
+import java.util.ArrayList;
+import java.util.Comparator;
+
+/**
+ * 문제 : 예산
+ * 링크 : https://programmers.co.kr/learn/courses/30/lessons/12982
+ */
+
 public class Budget {
     public static void main(String args[]){
 
+        //int[] d = {1, 3, 2, 5, 4};
+        //int budget = 9;  // result = 3
+
+        int[] d = {2, 2, 3, 3};
+        int budget = 10;  // result = 4
+
+        System.out.println(solution(d, budget));
     }
 
-    public int solution(int[] d, int bud) {
+
+    public static int solution(int[] d, int budget) {
         int answer = 0;
         
+        //배열로 쓰는 방법
+        // Arrays.sort(d) -> 정렬
+        
+        ArrayList<Integer> list = new ArrayList<>();
+
+        for(int num : d)
+            list.add(num);
+
+        list.sort(Comparator.naturalOrder());
+
+        for(int i=0; i<list.size(); i++){
+            if(list.get(i)<=budget){
+                budget-=list.get(i);
+                answer++;
+            }
+            else
+                return answer;
+        }
         return answer;
     }
 }
