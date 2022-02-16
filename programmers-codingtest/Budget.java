b7d25e97865469998cb191096a86306daca865c8
조민규 <inch1209@naver.com>
2022-02-16 23:53:03 +0900

    programmers-Budget
    https://programmers.co.kr/learn/courses/30/lessons/12982

---
+++ programmers-codingtest/Budget.java
@@ -0,0 +1,11 @@
+public class Budget {
+    public static void main(String args[]){
+
+    }
+
+    public int solution(int[] d, int bud) {
+        int answer = 0;
+        
+        return answer;
+    }
+}
