cd236110bf202e0d984b131476de9f744efeffd4
조민규 <inch1209@naver.com>
2022-02-18 23:58:21 +0900

    programmers-SumTwoNumbers
    https://programmers.co.kr/learn/courses/30/lessons/68644

---
+++ programmers-codingtest/SumTwoNumbers.java
@@ -0,0 +1,22 @@
+/**
+ * 문제 : 두 개 뽑아서 더하기
+ * 링크 : https://programmers.co.kr/learn/courses/30/lessons/68644
+ */
+
+public class SumTwoNumbers {
+    public static void main(String args[]){
+        int numbers[] = {2, 1, 3, 4, 1};
+        //int numbers[] = {5, 0, 2, 7};
+
+        int[] answer = solution(numbers);
+
+        for(int num : answer)
+            System.out.print(num + " ");
+    }
+
+    public static int[] solution(int[] numbers) {
+        int[] answer = {};
+
+        return answer;
+    }
+}
