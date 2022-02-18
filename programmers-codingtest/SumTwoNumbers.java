a7451a98c8ce8bef64cd19372d3be2282dccf08c
조민규 <inch1209@naver.com>
2022-02-19 00:48:42 +0900

    programmers-SumTwoNumbers
    https://programmers.co.kr/learn/courses/30/lessons/68644
    
    set은 중복x, set 사용+Collection.sort(set name)

--- programmers-codingtest/SumTwoNumbers.java
+++ programmers-codingtest/SumTwoNumbers.java
@@ -1,3 +1,7 @@
+import java.net.Inet4Address;
+import java.util.ArrayList;
+import java.util.Comparator;
+
 /**
  * 문제 : 두 개 뽑아서 더하기
  * 링크 : https://programmers.co.kr/learn/courses/30/lessons/68644
@@ -5,8 +9,8 @@
 
 public class SumTwoNumbers {
     public static void main(String args[]){
-        int numbers[] = {2, 1, 3, 4, 1};
-        //int numbers[] = {5, 0, 2, 7};
+        //int numbers[] = {2, 1, 3, 4, 1};
+        int numbers[] = {5, 0, 2, 7};
 
         int[] answer = solution(numbers);
 
@@ -16,6 +20,25 @@ public class SumTwoNumbers {
 
     public static int[] solution(int[] numbers) {
         int[] answer = {};
+        int tmp;
+        ArrayList<Integer> list = new ArrayList<>();
+
+
+        for(int i=0; i<numbers.length; i++){
+            for(int j=i+1; j<numbers.length; j++){
+                tmp = numbers[i]+numbers[j];
+                if(!list.contains(tmp))
+                    list.add(tmp);
+            }
+        }
+
+        list.sort(Comparator.naturalOrder());
+
+        answer = new int[list.size()];
+        for(int i=0; i<list.size(); i++){
+            answer[i]=list.get(i);
+        }
+
 
         return answer;
     }
