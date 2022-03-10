b919f4bfdd4a1f3c2faee601e1bb6a128ef01935
조민규 <inch1209@naver.com>
2022-03-10 17:07:09 +0900

    programmers-MinimumRectengle
    https://programmers.co.kr/learn/courses/30/lessons/86491
    
    min,max(Math 함수) 이용

--- programmers-codingtest/MinimumRectangle.java
+++ programmers-codingtest/MinimumRectangle.java
@@ -5,14 +5,17 @@
 
 public class MinimumRectangle {
     public static void main(String args[]){
-        int sizes[][] = {{60, 50}, {30, 70}, {60, 30}, {80, 40}};  // 4000
+        //int sizes[][] = {{60, 50}, {30, 70}, {60, 30}, {80, 40}};  // 4000
         //int sizes[][] = {{10, 7}, {12, 3}, {8, 15}, {14, 7}, {5, 15}};  // 120
-        //int sizes[][] = {{14, 4}, {19, 6}, {6, 16}, {18, 7}, {7, 11}};  // 133
+        int sizes[][] = {{14, 4}, {19, 6}, {6, 16}, {18, 7}, {7, 11}};  // 133
         System.out.println(solution(sizes));
 
     }
     public static int solution(int [][] sizes){
         int height = 0 , width = 0;
+
+        // 기존 풀인
+/*
         int[] tmp = new int[sizes.length];
 
         for (int i = 0; i < sizes.length; i++) {
@@ -25,10 +28,16 @@ public class MinimumRectangle {
             else
                 tmp[i] = sizes[i][1];
         }
-
         for(int n : tmp)
             if(n > width)
                 width = n;
+*/
+
+        // min,max(Math함수) 활용
+        for(int i=0; i<sizes.length; i++){
+            height = Math.max(height,(Math.max(sizes[i][0],sizes[i][1])));
+            width = Math.max(width,Math.min(sizes[i][0],sizes[i][1]));
+        }
 
         return width*height;
     }
