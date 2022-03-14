320460dbdb14fdb6c5a24574acb203e308578fcc
조민규 <inch1209@naver.com>
2022-03-14 23:06:12 +0900

    programmers-CountryOfOneTwoFour
    https://programmers.co.kr/learn/courses/30/lessons/12899
    
    문제 해결

--- programmers-codingtest/CountryOfOneTwoFour.java
+++ programmers-codingtest/CountryOfOneTwoFour.java
@@ -1,3 +1,5 @@
+import java.util.Stack;
+
 /**
  * 문제 : 124 나라의 숫자
  * 링크 : https://programmers.co.kr/learn/courses/30/lessons/12899
@@ -5,29 +7,48 @@
 
 public class CountryOfOneTwoFour {
     public static void main(String args[]){
-        int n = 1;  //1
+        //int n = 1;  //1
         //int n = 2;  //2
         //int n = 3;  //4
         //int n = 4;  //11
 
-        System.out.println(solution(n));
-
+        System.out.println(solution(1));
+        System.out.println(solution(2));
+        System.out.println(solution(3));
+        System.out.println(solution(4));
+        System.out.println(solution(5));
+        System.out.println(solution(6));
+        System.out.println(solution(7));
+        System.out.println(solution(8));
+        System.out.println(solution(9));
+        System.out.println(solution(10));
     }
     public static String solution(int n){
         String answer = "";
-        String tmpChar;
-        String tmpString ="";
 
-        tmpString = String.valueOf(n);
+        Stack<Integer> stack = new Stack<>();
 
-        System.out.println(answer.length());
+        while(true){
+            if(n%3==0) {
+                stack.push(3);
+                n = n/3-1;
+                if(n<=0)
+                    break;
+            }
+            else{
+                stack.push(n % 3);
+                n = n / 3;
+                if (n == 0)
+                    break;
+            }
+        }
+        while(!stack.isEmpty()){
+            answer += Integer.toString(stack.pop());
+        }
 
-        String[] num = {"10", "1", "4", "3", "5", "6", "7", "8", "9", "10"};
-        String[] countryNum = {"41",}
-
-
-        //char[] num = {'1','2','3','4','5','6','7','8','9'};
-        //char[] countryNum = {'1', '2', '4', '11', '12', '14','21','22','24','41'};
+        answer = answer.replaceAll("1", "1");
+        answer = answer.replaceAll("2", "2");
+        answer = answer.replaceAll("3", "4");
 
 
         return answer;
