71f7c6713c6dad11262388b8510304cf477b4f2a
조민규 <inch1209@naver.com>
2022-05-25 20:40:27 +0900

    baek joon start

---
+++ baekjoon-codingtest/DistributedProcessing.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+/**
+ * 문제 : 분산처리
+ * 링크 : https://www.acmicpc.net/problem/1009
+**/
+
+
+public class DistributedProcessing {
+
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+
+        int num = sc.nextInt();
+
+        
+    }
+}
