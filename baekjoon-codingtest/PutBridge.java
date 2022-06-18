dc0f74d8cdfcc514105a9e64859faa5e55746e10
조민규 <inch1209@naver.com>
2022-06-19 03:13:44 +0900

    Baekjoon-PutBridge
    combination solve

--- baekjoon-codingtest/PutBridge.java
+++ baekjoon-codingtest/PutBridge.java
@@ -1,6 +1,13 @@
+/**
+ * 문제 : 다리 놓기(1010)
+ * 링크 : https://www.acmicpc.net/problem/1010
+ **/
+
 import java.util.Scanner;
 
 public class PutBridge {
+    // 기존 풀이
+    /*
     public static void main(String args[]) {
         int dp[][] = new int[31][31];
 
@@ -37,7 +44,34 @@ public class PutBridge {
             int m = sc.nextInt();
             System.out.println(dp[n][m]);
         }
+    }*/
 
+    // 조합 사용
+    public static void main(String args[]){
+        int dp[][] = new int[31][31];
+
+        Scanner sc = new Scanner(System.in);
+
+        dp[0][0] = 0;
+
+        for(int i = 0; i < 31; i++) dp[1][i] = i;
+
+        // i = n => left site
+        for(int i = 2; i < 31; i++){
+            dp[i][i] = 1;
+            // j = m => right site
+            for(int j = i+1; j < 31; j++){
+                dp[i][j] = dp[i-1][j-1] + dp[i][j-1];
+            }
+        }
+
+        int t = sc.nextInt();
+
+        for(int i = 0; i < t; i++){
+            int n = sc.nextInt();
+            int m = sc.nextInt();
+            System.out.println(dp[n][m]);
+        }
 
     }
 }
