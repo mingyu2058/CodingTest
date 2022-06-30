2e2259129b6c5d5485e8c3107a0dbc41cafdb928
조민규 <inch1209@naver.com>
2022-07-01 01:38:31 +0900

    using twopoint

--- baekjoon-codingtest/SumOfNumber5.java
+++ baekjoon-codingtest/SumOfNumber5.java
@@ -2,15 +2,33 @@ import java.util.Scanner;
 
 public class SumOfNumber5 {
     public static void main(String args[]){
+        useTwoPoint();
+        /*
         Scanner sc = new Scanner(System.in);
 
+        //int arr[] = new int[n];
+        //long sum[] = new long[n];
+
         int n = sc.nextInt();
-        int arr[] = new int[n];
-
-        long sum[] = new long[n];
         int result = 0;
+        long previousSum = 0;
+        long presentSum = 0;
 
+        for(int i = 1; i <= n; i++){
+            if(presentSum + i > n)
+                break;
 
+            presentSum = previousSum + i;
+            if((n-presentSum) % i == 0)
+                result++;
+            previousSum = presentSum;
+        }
+        System.out.println(result);
+
+*/
+
+        // 초기 계산 - 메모리 초과
+        /*
         sum[0] = 0;
         long tmp = 0;
         for(int i = 1; i <= sum.length; i++){
@@ -21,15 +39,45 @@ public class SumOfNumber5 {
             }
             sum[i] = sum[i-1] + i;
         }
-
-
         for(int i = 2; i < tmp; i++){
             if((n-sum[i])%i==0) {
                 result++;
             }
         }
+         */
 
-        System.out.println(result+1);
 
+
+
+
+    }
+    static int useTwoPoint(){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int result = 0;
+        int startPoint = 1, endPoint = 1;
+
+        long sum = 1;
+
+        while(endPoint != n){
+            if(sum == n) {
+                result++;
+                endPoint++;
+                sum += endPoint;
+            }
+            else if(sum > n){
+                sum -= startPoint;
+                startPoint++;
+            }
+            else{
+                endPoint++;
+                sum += endPoint;
+            }
+            System.out.println("sum = " + sum);
+            System.out.println("endPoint = " + endPoint);
+        }
+
+
+        return result;
     }
 }
