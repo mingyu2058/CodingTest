4ee667daefe7e4db9aa3b4519449af84b0f0a27b
조민규 <inch1209@naver.com>
2022-07-05 21:27:39 +0900

    Baekjoon - Card2
    use stack

--- baekjoon-codingtest/Card2.java
+++ baekjoon-codingtest/Card2.java
@@ -5,6 +5,7 @@
 
 import java.util.LinkedList;
 import java.util.Queue;
+import java.util.Stack;
 import java.util.Scanner;
 
 public class Card2 {
@@ -34,6 +35,41 @@ public class Card2 {
 
         System.out.println(queue.poll());
     }
-    
+
+    // stack 사용
+    /*
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+
+        int n =  sc.nextInt();
+        int tmp = 0;
+
+        int[] arr = new int[n];
+        int arrPoint = 0;
+
+        Stack<Integer> stack = new Stack<>();
+
+        for(int i = n; i >= 1; i--){
+            stack.push(i);
+        }
+
+
+        while(stack.size() != 1) {
+            arrPoint = 0;
+            while (!stack.isEmpty()) {
+                if (tmp++ % 2 == 0) {
+                    stack.pop();
+                } else {
+                    arr[arrPoint++] = stack.pop();
+                }
+            }
+            for(int j = arrPoint-1; j >= 0; j--){
+                stack.push(arr[j]);
+            }
+        }
+        System.out.println(stack.pop());
+
+    }
+*/
 
 }
