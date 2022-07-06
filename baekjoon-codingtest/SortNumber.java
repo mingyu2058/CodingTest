7362d5af83a3477185d6bc75325d7d65a30dff59
조민규 <inch1209@naver.com>
2022-07-07 02:55:55 +0900

    Baekjoon - SortNumber
    Select Sort, Bubble Sort

--- baekjoon-codingtest/SortNumber.java
+++ baekjoon-codingtest/SortNumber.java
@@ -2,8 +2,6 @@
  * 문제 : 수 정렬하기
  * 링크 : https://www.acmicpc.net/problem/2750
  **/
-
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -19,7 +17,38 @@ public class SortNumber {
             arr[i] = Integer.parseInt(bf.readLine());
         }
 
+        // Use Arrays.sort
+        /*
         Arrays.sort(arr);
+        */
+
+        // Use Select sort
+        /*
+        int tmp;
+        for(int i = 0; i < n-1; i++){
+            for(int j = i+1; j < n; j++){
+                if(arr[i] > arr[j]){
+                    tmp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = tmp;
+                }
+            }
+        }
+        */
+
+        // Use Bubble sort
+        int tmp;
+        for(int j = n-1; j >=1; j--) {
+            for (int i = 0; i < j; i++) {
+                if (arr[i] > arr[i + 1]) {
+                    tmp = arr[i + 1];
+                    arr[i + 1] = arr[i];
+                    arr[i] = tmp;
+                }
+            }
+        }
+
+
         for(int i = 0; i < n; i++){
             System.out.println(arr[i]);
         }
