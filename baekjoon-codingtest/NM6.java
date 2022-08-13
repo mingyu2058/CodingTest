5a687868a77f55e4d7b6d7c8f036253f00c82bd1
조민규 <inch1209@naver.com>
2022-08-13 22:00:17 +0900

    Baekjoon - NM6

---
+++ baekjoon-codingtest/NM6.java
@@ -0,0 +1,59 @@
+import java.io.*;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class NM6 {
+    static int N,M,Count;
+    static int arr[];
+    static BufferedWriter bw;
+
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer tokenizer = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(tokenizer.nextToken());
+        M = Integer.parseInt(tokenizer.nextToken());
+        arr = new int[N];
+
+        tokenizer = new StringTokenizer(br.readLine());
+        for(int i = 0; i < N; i++){
+            arr[i] = Integer.parseInt(tokenizer.nextToken());
+        }
+        Arrays.sort(arr);
+
+        for(int i = 0; i < N; i++){
+            dfs(i,"");
+        }
+
+        bw.flush();
+        bw.close();
+    }
+    static void dfs(int num,String s) throws IOException {
+        // 체크인
+        s += arr[num];
+        Count++;
+
+        // 목적지 확인
+        if(Count == M){
+            s += "\n";
+            bw.write(s);
+        }
+
+        s += " ";
+
+        // 순회
+        for(int i = num+1; i < N; i++){
+            // 갈 수 있는가?
+            if( Count < N ) {
+                // 간다
+                dfs(i, s);
+            }
+        }
+
+        // 체크아웃
+        Count--;
+
+    }
+
+}
