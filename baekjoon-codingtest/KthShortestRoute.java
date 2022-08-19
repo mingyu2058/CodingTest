d1e09252fdf6ddf5b04c5205046f2353bcff08e2
조민규 <inch1209@naver.com>
2022-08-19 22:43:14 +0900

    Baekjoon - KthShortestRoute
    explain

--- baekjoon-codingtest/KthShortestRoute.java
+++ baekjoon-codingtest/KthShortestRoute.java
@@ -36,32 +36,50 @@ public class KthShortestRoute {
     }
 
     private static void dijkstra() {
+        // PriorityQueue 가중치 오름차순 정렬
         PriorityQueue<Route> pq = new PriorityQueue<Route>();
         pq.offer(new Route(1, 0));
         int doneCnt = 0;
-        while(!pq.isEmpty()) {
+        while (!pq.isEmpty()) {
+            // 현재 Vertex 뽑기 
             Route curr = pq.poll();
+
+            // Vertex의 크기가 K보다 작을 때 Vertex 에다가 가중치 추가
             if (dist[curr.v].size() < K) dist[curr.v].add(curr.w);
             else continue;
+
+            // vertext의 크기가 K랑 같다면 카운트 증가
             if (dist[curr.v].size() == K) doneCnt++;
+
+            // 카운트가 N이랑 같다면 모든 순회 끝 -> 반복문 종료
             if (doneCnt == N) break;
-            for(int[] next : adj[curr.v]) {
+
+            // vertex의 인접 리스트 => next
+            for (int[] next : adj[curr.v]) {
+                // next의 vertex
                 int v = next[0];
+                // next의 가중치
                 int c = next[1];
+
+                // 새로 추가될 가중치 ( next이전의 curr의 가중치 + next의 가중치 )
                 int tc = curr.w + c;
+
+                // curr의 인접 리스트 next의 크기가 K 보다 작다면
                 if (dist[v].size() < K) {
                     pq.offer(new Route(v, tc));
                 }
             }
         }
     }
-    public static class Route implements Comparable<Route>{
+    public static class Route implements Comparable<Route> {
         int v, w;
+
         public Route(int v, int w) {
             super();
             this.v = v;
             this.w = w;
         }
+
         @Override
         public int compareTo(Route o) {
             return this.w - o.w;
