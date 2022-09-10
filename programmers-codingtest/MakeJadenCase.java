dbb77de42279bf5c9a59afd2ef2d0eb343ba1d2b
조민규 <inch1209@naver.com>
2022-09-10 17:16:28 +0900

    programmers - MakeJadenCase

---
+++ programmers-codingtest/MakeJadenCase.java
@@ -0,0 +1,37 @@
+import java.util.StringTokenizer;
+
+public class MakeJadenCase {
+    static StringBuilder sb;
+    public static void main(String args[]){
+        //String s = "3people unFollowed me";
+        String s = "for the last week";
+
+        String print = solution(s);
+        System.out.println("print = " + print);
+    }
+    static String solution(String s){
+        sb = new StringBuilder();
+        StringTokenizer st = new StringTokenizer(s);
+        boolean isCheck;
+
+        while(st.hasMoreTokens()){
+            String cur = st.nextToken();
+            isCheck = true;
+
+            for(int i = 0; i < cur.length(); i++){
+                char c = cur.charAt(i);
+                check(c,isCheck);
+                
+
+            }
+
+
+        }
+        return answer;
+    }
+
+    static char check(char c, boolean isCheck){
+
+    }
+
+}
